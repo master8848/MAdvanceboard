bb0f148d22a8a527ccd25b66abdbd512bae2d880
Saurav sanjel <jobarsanjel2@gmail.com>
2026-09-12 08:15:26 +0545

    feat(android): gradle shell + settings

---
+++ android/settings.gradle.kts
@@ -0,0 +1,20 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.name = "kb-ime"
+include(":app")
+include(":ime")
+include(":plugin-api")
+include(":core-bridge")
+include(":sync")
